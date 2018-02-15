82e52566d6e6d024c910cdf22dbcf4a2b1fe15b9
Ciaran Davern <117384231@umail.ucc.ie>
2018-02-15 10:10:18 +0000

    adding css to ebus3

--- ebusiness/Ebus3.php
+++ ebusiness/Ebus3.php
@@ -3,15 +3,23 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html>
+    
+     <link rel="stylesheet" href="../mystylesheet.css" type="text/css"/>
     <head>
         <title>Receipt</title>
+        <center>
+            <div class="header">
+  <h1 class="mainheading">Receipt</h1>
+</div>
+        
     </head>
     <body>
-        <h4>Receipt</h4>
+        
         
         <?php
         //Echo session variables that were set on previous page
         echo "Total is " . $_SESSION["total"] . ".";
         ?>
     </body>
+    </center>
 </html>
